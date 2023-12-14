75b2f0499c21d43a3af3ff902b13657ce2d9d82a
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-12-14 23:57:32 +0200

    2

--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -6,7 +6,7 @@ const {sendMailResetPassword, sendMailServiceMassage} = require("../../utils/sen
 const {validationResult} = require("express-validator");
 const {secret} = require("../../userConfig");
 const generateRandomPassword = require("../../utils/generationPassword/generationPassword");
-const {URI} = require("../../config");
+const URI = require("../../config");
 
 
 const generationToken = (id) =>{
@@ -206,7 +206,8 @@ const activityPassword = async (req, res) => {
         { $set: { password: hashPassword, env:"" } }
     );
     console.log('Password is activated.');
-    return res.redirect(URI);
+    // return res.redirect(URI);
+    return res.redirect("https://www.google.com/");
 };
 
 
